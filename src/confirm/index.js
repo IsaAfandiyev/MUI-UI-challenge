e59e6a330547c0d7e643f69b4823e8b08d733198
IsaAfandiyev <isa.efendiyev@gmail.com>
2022-12-27 22:49:56 +0400

    prettier

--- src/confirm/index.js
+++ src/confirm/index.js
@@ -1,12 +1,17 @@
 import "./index.css";
 function ConfirmDelete(props) {
-    return <div className='question'>
-        <div className="container">
-            <h3 className='header'>Delete this ? </h3>
-            <button className='yesBtn' onClick={()=>props.question(true)}>Yes</button>
-            <button className='noBtn' onClick={()=>props.question(false)}>No</button>
-        </div>
-
+  return (
+    <div className="question">
+      <div className="container">
+        <h3 className="header">Delete this ? </h3>
+        <button className="yesBtn" onClick={() => props.question(true)}>
+          Yes
+        </button>
+        <button className="noBtn" onClick={() => props.question(false)}>
+          No
+        </button>
+      </div>
     </div>
+  );
 }
 export default ConfirmDelete;
